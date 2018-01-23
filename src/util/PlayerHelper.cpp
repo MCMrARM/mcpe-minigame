cea119278b7df99d220cfa39c7166e2664c6312d
MCMrARM <mcmrarm@gmail.com>
2018-01-23 21:14:56 +0100

    PlayerHelper: add a fast network identifier => player lookup map

--- src/util/PlayerHelper.cpp
+++ src/util/PlayerHelper.cpp
@@ -3,6 +3,7 @@
 #include <minecraft/server/ServerNetworkHandler.h>
 #include <minecraft/entity/Player.h>
 #include "../statichook.h"
+#include "Log.h"
 
 PlayerHelper PlayerHelper::instance;
 
@@ -28,6 +29,35 @@ void PlayerHelper::onPlayerLeft(Player& player) {
     playerData.erase(&player);
 }
 
+void PlayerHelper::onPlayerConnected(NetworkIdentifier const& nid, Player& player) {
+    Log::trace("PlayerHelper", "Player connected from %s", nid.toString().c_str());
+    playerFastLookupTable[nid].push_back(&player);
+}
+
+void PlayerHelper::onPlayerDisconnected(NetworkIdentifier const& nid) {
+    playerFastLookupTable.erase(nid);
+    Log::trace("PlayerHelper", "Player disconnected (%s)", nid.toString().c_str());
+}
+
+Player* PlayerHelper::findNetPlayer(NetworkIdentifier const& nid, unsigned char subId) {
+    if (playerFastLookupTable.count(nid) < 0)
+        return nullptr;
+    auto const& players = playerFastLookupTable.at(nid);
+    for (Player* player : players) {
+        if (player->getClientSubId() == subId)
+            return player;
+    }
+    return nullptr;
+}
+
+class ConnectionRequest;
+TInstanceHook(ServerPlayer*, _ZN20ServerNetworkHandler16_createNewPlayerERK17NetworkIdentifierRK17ConnectionRequest,
+              ServerNetworkHandler, NetworkIdentifier const& nid, ConnectionRequest const& req) {
+    ServerPlayer* ret = original(this, nid, req);
+    PlayerHelper::instance.onPlayerConnected(nid, *ret);
+    return ret;
+}
+
 TInstanceHook(void, _ZN20ServerNetworkHandler24onReady_ClientGenerationER6PlayerRK17NetworkIdentifier,
               ServerNetworkHandler, Player& player, NetworkIdentifier const& nid) {
     original(this, player, nid);
@@ -39,4 +69,10 @@ TInstanceHook(void, _ZN20ServerNetworkHandler13_onPlayerLeftEP12ServerPlayer,
     original(this, player);
     if (player != nullptr)
         PlayerHelper::instance.onPlayerLeft(*player);
+}
+
+TInstanceHook(void, _ZN20ServerNetworkHandler12onDisconnectERK17NetworkIdentifierRKSsb,
+              ServerNetworkHandler, NetworkIdentifier const& nid, std::string const& str, bool b) {
+    original(this, nid, str, b);
+    PlayerHelper::instance.onPlayerDisconnected(nid);
 }
--- src/util/PlayerHelper.h
+++ src/util/PlayerHelper.h
@@ -2,6 +2,8 @@
 
 #include <unordered_set>
 #include <unordered_map>
+#include <vector>
+#include <minecraft/net/NetworkIdentifier.h>
 #include "PlayerData.h"
 
 class Player;
@@ -23,6 +25,7 @@ private:
     friend class PlayerEventListener;
 
     std::unordered_map<Player*, PlayerData> playerData;
+    std::unordered_map<NetworkIdentifier, std::vector<Player*>> playerFastLookupTable;
     std::unordered_set<PlayerEventListener*> listeners;
 
     void registerListener(PlayerEventListener* listener) {
@@ -39,7 +42,12 @@ public:
         return playerData.at(&player);
     }
 
+    Player* findNetPlayer(NetworkIdentifier const& nid, unsigned char subId);
+
     void onPlayerJoined(Player& player);
     void onPlayerLeft(Player& player);
+
+    void onPlayerConnected(NetworkIdentifier const& nid, Player& player);
+    void onPlayerDisconnected(NetworkIdentifier const& nid);
 
 };
