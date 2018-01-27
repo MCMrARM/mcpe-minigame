30f8fb4d2b5d5201f634916c35fd8d9c2734fa23
MCMrARM <mcmrarm@gmail.com>
2018-01-27 13:19:36 +0100

    Death messages

---
+++ src/minigame/MinigamePlayerHooks.cpp
@@ -0,0 +1,20 @@
+#include "../statichook.h"
+
+#include <cstdlib>
+#include <minecraft/entity/Player.h>
+#include <minecraft/entity/EntityDamageSource.h>
+#include <minecraft/lang/I18n.h>
+#include "../lobby/LobbyManager.h"
+#include "Minigame.h"
+
+TInstanceHook(void, _ZN6Player3dieERK18EntityDamageSource, Player, EntityDamageSource const& src) {
+    //original(this, src);
+    serializationSetHealth(20);
+    PlayerData& playerData = PlayerHelper::instance.getPlayerData(*this);
+    if (playerData.currentMinigame != nullptr) {
+        auto msg = src.getDeathMessage(getNameTag(), this);
+        playerData.currentMinigame->broadcast("§c" + I18n::get(msg.message, msg.args));
+        playerData.currentMinigame->removePlayer(this);
+    }
+    LobbyManager::instance.sendPlayerToLobby(*this);
+}
